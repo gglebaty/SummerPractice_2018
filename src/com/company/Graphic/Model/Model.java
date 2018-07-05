4560f0df7aeb36f52b54bb00521280af933332b7
yar1k112 <yarik1998@gmail.com>
2018-07-05 16:57:29 +0300

    one more

--- src/com/company/Graphic/Model/Model.java
+++ src/com/company/Graphic/Model/Model.java
@@ -29,15 +29,13 @@ public class Model {
             for (int k = 0; k < squares.size(); k++) {
                 Field condition = table.makeCopy();
                 Square tmp = squares.get(k);
-                if (!tmp.isInField())
+                if (!tmp.isInField() && !wasFound)
                 {
                     if(condition.isPossible(i,j,tmp))
                     {
                         condition.insertSquare(tmp, i, j);
                         tmp.setInField(true);
-                        if (!wasFound) {
-                            addNewCondition(squares);
-                        }
+                        addNewCondition(squares);
                         if (condition.isFreeCell(i, j)) {
                             go(condition, condition.getNextI(i, j), condition.getNextJ(i, j));//go to free cell
                         } else {
